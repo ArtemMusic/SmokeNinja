86f28a90c03dec2be171097736e68eabf14189df
Artem Muzychuk <50505906+ArtemMusic@users.noreply.github.com>
2022-10-09 15:00:18 +0300

    fix: исправил отправку пустого имени при создании категории Теперь если не написать название для категории и отправить на создание, то система выдведет ошибку необходимости заполнения поля

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -2,41 +2,44 @@
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Добавление категории</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#">Категории</a></li>
-                                <li class="breadcrumb-item active">Blog</li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Добавление категории</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Категории</a></li>
+                        <li class="breadcrumb-item active">Blog</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <!-- Small boxes (Stat box) -->
-                    <div class="row">
-                        <div class="col-12">
-                            <form action="{{route('admin.category.store')}}" class="w-25" method="POST">
-                                @csrf
-                                <input  name="title" type="text" class="form-control" placeholder="Название">
-                                <button type="submit" class="mt-3 btn btn-block btn-outline-success">Создать</button>
-                                <button type="reset" class="btn btn-block btn-outline-secondary">Сбросить</button>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-        </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-12">
+                    <form action="{{route('admin.category.store')}}" class="w-25" method="POST">
+                        @csrf
+                        <input id="title" name="title" type="text" class="form-control" placeholder="Название">
+                        @error('title')
+                            <p class="text-danger">Поле необходимо для заполнения</p>
+                        @enderror
+                        <button type="submit" class="mt-3 btn btn-block btn-outline-success">Создать</button>
+                        <button type="reset" class="btn btn-block btn-outline-secondary">Сбросить</button>
+                    </form>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
 @endsection
