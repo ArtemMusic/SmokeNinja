7d5d044c5f2f3c198573ba1c88bcadce8e575167
Artem Muzychuk <50505906+ArtemMusic@users.noreply.github.com>
2022-10-12 21:41:24 +0300

    fix: переделанны ссылки на странице для тегов и 1 ссылка для категории На страницу edit,create,show для тегов и на странице edit для категорий (см сверху справа)

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -11,8 +11,8 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('admin.index')}}">Главная</a></li>
-                        <li class="breadcrumb-item active">AdminPanel</li>
+                        <li class="breadcrumb-item"><a href="{{route('admin.category.index')}}">Категории</a></li>
+                        <li class="breadcrumb-item active">CategoryList</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
--- resources/views/admin/tags/create.blade.php
+++ resources/views/admin/tags/create.blade.php
@@ -11,8 +11,8 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('admin.tag.index')}}">Категории</a></li>
-                        <li class="breadcrumb-item active">TaglIST</li>
+                        <li class="breadcrumb-item"><a href="{{route('admin.tag.index')}}">Теги</a></li>
+                        <li class="breadcrumb-item active">TagList</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
