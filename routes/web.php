1b43a15c88151c3f7ea63e9a96cfe0acc0530026
Artem Muzychuk <50505906+ArtemMusic@users.noreply.github.com>
2022-10-20 23:24:56 +0300

    feat: добавил Middleware для доступа к админпанели Теперь чтобы попасть в админку нужно авторизироваться Добавил кнопку выхода на панели Подключил систему auth()

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('main.index');
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin']], function () {
     Route::get('/', 'IndexController')->name('admin.index');
 
     //Категории
