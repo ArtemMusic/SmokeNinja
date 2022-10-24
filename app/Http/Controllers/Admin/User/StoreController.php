7724cc2b84a26dbdac751fae383dca2f87fae5cd
Artem Muzychuk <50505906+ArtemMusic@users.noreply.github.com>
2022-10-24 13:33:22 +0300

    feat: добавил отправку письма на почту при создании юзера через админку Отправка происходит через сервис mailtrap, который интегрируется с локальной средой Добавил класс для отправки пароля на почту Поменял реквесты Поменял сервис

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -10,7 +10,6 @@ class StoreController extends BaseController
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $data['password'] = Hash::make($data['password']);
         $this->service->store($data);
         return redirect()->route('admin.user.index');
     }
--- app/Http/services/Admin/User/Service.php
+++ app/Http/services/Admin/User/Service.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\services\Admin\User;
 
+use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class Service
 {
     public function store($data)
     {
+        $password = Str::random(10);
+        $data['password'] = Hash::make($password);
         $user = User::FirstOrCreate(["email" => $data['email']], $data);
+        Mail::to($data['email'])->send(new PasswordMail($password));
         return $user;
     }
 
