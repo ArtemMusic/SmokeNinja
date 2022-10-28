36735b7c05199de8df13ac573d1b5747d75c75b7
Artem Muzychuk <50505906+ArtemMusic@users.noreply.github.com>
2022-10-28 22:04:29 +0300

    feat: Перевел страницу авторизиции, сделал отображение админа в панели 1) Все страницы, относящиеся к авторизации и верификации были локализированы 2) Теперь в админпанели отображается логин админа

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,7 +7,7 @@
                 <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">{{Auth::user()->name}}</a>
             </div>
         </div>
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
